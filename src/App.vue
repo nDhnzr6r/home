a41e6f574bc09c05f79ddf2d822dc7ee83fb7760
nDhnzr6r <141411248+nDhnzr6r@users.noreply.github.com>
2025-05-26 02:48:37 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -117,7 +117,7 @@ onMounted(() => {
   | | | |\\/| |\\___ \\  \\   /    \\   /
  _| |_| |  | |____) |  | |      | |
 |_____|_|  |_|_____/   |_|      |_|`;
-  const content = `\n\n版本: ${config.version}\n主页: ${config.home}\nGithub: ${config.githubnd}`;
+  const content = `\n\n版本: ${config.version}\n主页: ${config.home}\nGithub: ${config.github}`;
   console.info(`%c${title1} %c${title2} %c${content}`, styleTitle1, styleTitle2, styleContent);
 });
 
